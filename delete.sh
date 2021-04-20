581f386e965e5b978e1cd5e175ac2a0273f5f441
iciamyplant <emma.bourdit@gmail.com>
2021-04-20 17:33:23 +0200

    tout n'est pas bon putain :'''''(

--- delete.sh
+++ delete.sh
@@ -1,2 +1,2 @@
-kubectl delete deployments --all ; kubectl delete services --all ; kubectl delete pods --all
+kubectl delete deployments --all ; kubectl delete services --all ; kubectl delete pods --all ; kubectl delete pvc --all
 minikube stop ; minikube delete
