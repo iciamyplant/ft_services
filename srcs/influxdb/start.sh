ac96ec94a6c0b607fed893af0ea7a13556595d88
iciamyplant <emma.bourdit@gmail.com>
2021-04-21 13:47:01 +0200

    tout semble ok cette fois

--- srcs/influxdb/start.sh
+++ srcs/influxdb/start.sh
@@ -1,3 +1,6 @@
+service influxdb start
+sleep 5
 echo "create database telegraf" | influx
 echo "create user telegrafuser with password 'password'" | influx
 echo "grant ALL on telegraf to telegrafuser" | influx
+sleep infinity
